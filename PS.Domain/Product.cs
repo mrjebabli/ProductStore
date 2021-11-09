eef440a6f2b9d5ca69885561c32c0a21b72468e2
Mohamed Riadh JEBABLI <64383508+mrjebabli@users.noreply.github.com>
2021-11-09 17:00:40 +0100

    CodeSource - Séance 2+3

---
+++ PS.Domain/Product.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PS.Domain
+{
+    public class Product
+    {
+        public static int Compteur { get; set; }
+        
+        public Product()
+        {
+            Compteur++;
+        }
+        public Product(DateTime DateProd, string description, string name, double price, int quantity, int productId)
+        {
+            this.DateProd = DateProd;
+            Description = description;
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+            ProductId = productId;
+            Compteur++;
+        }
+
+        public DateTime DateProd { get; set; }
+        //attribut
+        public  string Description { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public int ProductId
+        {
+            set;
+            get;
+        }
+
+        public override string ToString()
+        {
+            return ProductId + " " + Name 
+                + " " + Price + " " + DateProd + " " + Quantity;
+                ;
+        }
+    }
+
+    
+}
---
+++ PS.Domain/Provider.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PS.Domain
+{
+    public class Provider
+    {
+        private string password;
+        private string confirmPassword;
+
+
+
+        public static void SetIsApproved(bool x, Provider p)
+        {
+            //p.IsApproved = p.ConfirmPassword == p.password ? true : false;
+            x = p.ConfirmPassword.Equals( p.password);
+            Console.WriteLine("+++ "+x);
+            //if (p.ConfirmPassword == p.password)
+            //{
+            //    p.IsApproved = true;
+            //}
+            //else
+            //{
+            //    p.IsApproved = false;
+            //}
+        }
+        public string ConfirmPassword
+        {
+            get { return confirmPassword; }
+            set
+            {
+                if (value.Equals( Password))
+                {
+                    confirmPassword = value;
+                }
+                else { Console.WriteLine("confirmPassword!=password"); }
+            }
+        }
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                if (value?.Length >= 5 && value?.Length < 20)
+                {
+                    password = value;
+                }
+                else
+                {
+                    Console.WriteLine("Password >20");
+                }
+            }
+        }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public int Id { get; set; }
+        public bool IsApproved { get; set; }
+        public DateTime DateCreation { get; set; }
+    }
+}
